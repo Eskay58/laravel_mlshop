cccdcc99a5a56d940e963512ae3e7699d0df6d83
Eskay58 <say.yeah58@gmail.com>
2022-10-12 12:02:32 +0900

    属性バッグ

--- resources/views/tests/component-test1.blade.php
+++ resources/views/tests/component-test1.blade.php
@@ -2,9 +2,9 @@
     <x-slot name="header">
         ヘッダー１
     </x-slot>
-コンポーネントテスト１
-
-<x-tests.card title="タイトル1" content="本文1" :message="$message"></x-tests.card>
+    コンポーネントテスト１
+    <x-tests.card title="タイトル1" content="本文1" :message="$message"></x-tests.card>
     <x-tests.card title="タイトル1"></x-tests.card>
+    <x-tests.card title="CSS変更" class="bg-red-300"></x-tests.card>
 
 </x-tests.app>
