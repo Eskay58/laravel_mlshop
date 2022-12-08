7c0be1812bc9dd1ba5b108106ad5590b55820387
Eskay58 <say.yeah58@gmail.com>
2022-12-08 13:36:25 +0900

    view

--- resources/views/user/index.blade.php
+++ resources/views/user/index.blade.php
@@ -10,17 +10,16 @@
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 bg-white border-b border-gray-200">
                     <x-flash-message :status="session('status')" />
-                    {{-- <div class="flex justify-end mb-4">
-                        <button onclick="location.href='{{ route('owner.products.create') }}'"" class="text-white bg-indigo-500 border-0 py-2 px-8 focus:outline-none hover:bg-indigo-600 rounded text-lg">新規登録</button>
-                    </div> --}}
                     <div class="flex flex-wrap">
                         @foreach ($products as $product)
                             <div class="w-1/4 p-2 md:p-4">
                                 <a href="">
                                     <div class="border rounded-md p-2 md:p-4">
                                         <x-thumbnail filename="{{ $product->imageFirst->filename ?? ''}}" type="products" />
-                                        <div class="text-gray-700">
-                                            {{ $product->name }}
+                                        <div class="mt-4">
+                                            <h3 class="text-gray-500 text-xs tracking-widest title-font mb-1">{{ $product->category->name }}</h3>
+                                            <h2 class="text-gray-900 title-font text-lg font-medium">{{ $product->name }}</h2>
+                                            <p class="mt-1">{{ number_format($product->price) }}<span class="text-sm text-gray-700">円(税込)</span></p>
                                         </div>
                                     </div>
                                 </a>    
