b7e9d341d8d8f24d31fe8ba4cca5faa6a1c7a193
Eskay58 <say.yeah58@gmail.com>
2022-12-15 12:34:31 +0900

    カテゴリーで絞り込み

--- app/Http/Controllers/User/ItemController.php
+++ app/Http/Controllers/User/ItemController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\User;
 use App\Http\Controllers\Controller;
 use App\Models\Product;
 use App\Models\Stock;
+use App\Models\PrimaryCategory;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 
@@ -29,11 +30,14 @@ class ItemController extends Controller
 
     public function index(Request $request)
     {
+        $categories = PrimaryCategory::with('secondary')->get();
+
         $products = Product::availableItems()
+        ->selectCategory($request->category ?? '0')
         ->sortOrder($request->sort)
         ->paginate($request->pagination ?? '20');
 
-        return view('user.index', compact('products'));
+        return view('user.index', compact('products', 'categories'));
     }
 
     public function show($id)
