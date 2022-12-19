3854aa213bb3b83235c036ab328207ffb811e8bc
Eskay58 <say.yeah58@gmail.com>
2022-12-19 18:26:23 +0900

    ownerMail send process

--- app/Jobs/SendOrderedMail.php
+++ app/Jobs/SendOrderedMail.php
@@ -2,6 +2,7 @@
 
 namespace App\Jobs;
 
+use App\Mail\OrderedMail;
 use Illuminate\Bus\Queueable;
 use Illuminate\Contracts\Queue\ShouldBeUnique;
 use Illuminate\Contracts\Queue\ShouldQueue;
@@ -9,19 +10,17 @@ use Illuminate\Foundation\Bus\Dispatchable;
 use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Queue\SerializesModels;
 use Illuminate\Support\Facades\Mail;
-use App\Mail\TestMail;
-use App\Mail\ThanksMail;
 
-class SendThanksMail implements ShouldQueue
+class SendOrderedMail implements ShouldQueue
 {
     use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
 
-    public $products;
+    public $product;
     public $user;
 
-    public function __construct($products, $user)
+    public function __construct($product, $user)
     {
-        $this->products = $products;
+        $this->product = $product;
         $this->user = $user;
     }
 
@@ -32,8 +31,7 @@ class SendThanksMail implements ShouldQueue
      */
     public function handle()
     {
-        // Mail::to('test@example.com')->send(new TestMail());
-        Mail::to($this->user)
-        ->send(new ThanksMail($this->products, $this->user));
+        Mail::to($this->product['email'])
+        ->send(new OrderedMail($this->product, $this->user));
     }
 }
